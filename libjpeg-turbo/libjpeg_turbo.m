aef96d1b0697467e3b6bc1e93ae051d1af2ea628
Johannes Schriewer (Dunkelstern) <jschriewer@gmail.com>
2012-11-03 21:32:52 +0100

    Initial Commit

---
+++ libjpeg-turbo/libjpeg_turbo.m
@@ -0,0 +1,13 @@
+//
+//  libjpeg_turbo.m
+//  libjpeg-turbo
+//
+//  Created by Johannes Schriewer on 2012-11-03.
+//  Copyright (c) 2012 Johannes Schriewer. All rights reserved.
+//
+
+#import "libjpeg_turbo.h"
+
+@implementation libjpeg_turbo
+
+@end
